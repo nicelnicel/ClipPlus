8bd0f2521f474ec40d331d821a33722aa1c4d246
cc <fuvkoff@gg.com>
2026-06-12 22:32:07 +0800

    chore: bump version to v0.1.4

--- apps/mac/Sources/ClipPlusMac/CoreBridge/CoreBridge.swift
+++ apps/mac/Sources/ClipPlusMac/CoreBridge/CoreBridge.swift
@@ -9,7 +9,7 @@ struct FileTreeDownloadResult: Decodable, Equatable {
 
 struct CoreBridge {
     func statusJSON() -> String {
-        #"{"core_version":"0.1.3"}"#
+        #"{"core_version":"0.1.4"}"#
     }
 
     func deriveGroupId(for rawKey: String) -> String? {
